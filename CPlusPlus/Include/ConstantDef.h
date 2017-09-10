f8d151ebd5b376b3c452576e3ee8a8109967447b
meixi <a18772316113@163.com>
2017-09-10 12:16:15 +0800

    Initial commit

---
+++ CPlusPlus/Include/ConstantDef.h
@@ -0,0 +1,25 @@
+#ifndef __CONSTANTDEF_H__
+#define __CONSTANTDEF_H__
+
+namespace plusFCL_BTL
+{
+	//Media path Mgr.. 
+	//Format + T/S + 1/2 + classId
+	const char* Format_RGB = "RGB";
+	const char* Format_I420 = "I420";
+	const char* Format_H264 = "H264";
+	const char* Format_IPCMAINH264 = "IPCMAINH264";
+	const char* Format_IPCSUBH264 = "IPCSUBH264";
+	const char* Format_ARGB = "ARGB";
+	const char* Format_YUYV = "YUYV";
+	const char* Format_MIXRGB = "MIX_RGB";
+	const char* Format_CUTRGB = "CUT_RGB";
+	const char* Format_QAVMain = "QavMain";
+	const char* Format_QAVSub = "QavSub";
+	const char* Format_WEBRTC = "WEBRTC";
+
+	//
+
+}
+
+#endif
---
+++ CPlusPlus/Include/MacDef.h
@@ -0,0 +1,21 @@
+#ifndef __MACDEF_H__
+#define __MACDEF_H__ 
+
+namespace plusFCL_BTL
+{
+  /* @Detail: InI File
+  */
+	
+  /* @Detail Xml File
+  */
+
+  /* @Detail JSON File
+  */
+  
+  /* @Block Mgr
+  */
+
+  
+}
+
+#endif
