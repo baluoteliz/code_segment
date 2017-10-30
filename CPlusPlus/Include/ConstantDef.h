1dc25d2f893df10ad54f151b60f54834dcd2a338
meixi <a18772316113@163.com>
2017-10-30 15:20:27 +0800

    fix include files MacDef.h , ConstantDef.h

--- CPlusPlus/Include/ConstantDef.h
+++ CPlusPlus/Include/ConstantDef.h
@@ -5,21 +5,32 @@ namespace plusFCL_BTL
 {
 	//Media path Mgr.. 
 	//Format + T/S + 1/2 + classId
-	const char* Format_RGB = "RGB";
-	const char* Format_I420 = "I420";
-	const char* Format_H264 = "H264";
-	const char* Format_IPCMAINH264 = "IPCMAINH264";
-	const char* Format_IPCSUBH264 = "IPCSUBH264";
-	const char* Format_ARGB = "ARGB";
-	const char* Format_YUYV = "YUYV";
-	const char* Format_MIXRGB = "MIX_RGB";
-	const char* Format_CUTRGB = "CUT_RGB";
-	const char* Format_QAVMain = "QavMain";
-	const char* Format_QAVSub = "QavSub";
-	const char* Format_WEBRTC = "WEBRTC";
+	const char* const Format_RGB = "/RGB/";
+	const char* const Format_I420 = "/I420/";
+	const char* const Format_H264 = "/H264/";
+	const char* const Format_IPCMAINH264 = "/IPCMAINH264/";
+	const char* const Format_IPCSUBH264 = "/IPCSUBH264/";
+	const char* const Format_ARGB = "/ARGB/";
+	const char* const Format_YUYV = "/YUYV/";
+	const char* const Format_MIXRGB = "/MIX_RGB/";
+	const char* const Format_CUTRGB = "/CUT_RGB/";
+	const char* const Format_QAVMain = "/QavMain/";
+	const char* const Format_QAVSub = "/QavSub/";
+	const char* const Format_WEBRTC = "/WEBRTC/";
 
-	//
+	const char* const Video_Teacher = "t/";
+	const char* const Video_Student = "s/";
+	const char* const Video_Teacher1 = "t/1/";
+	const char* const Video_Teacher2 = "t/2/";
+	const char* const Video_Teacher3 = "t/3/";
+	const char* const Video_Student1 = "s/1/";
+	const char* const Video_Student2 = "s/2/";
+	const char* const Video_Student3 = "s/3/";
+	const char* const Video_VGA = "vga/";
 
+	
+	//¥ÌŒÛ√Ë ˆ.
+	
 }
 
 #endif
--- CPlusPlus/Include/MacDef.h
+++ CPlusPlus/Include/MacDef.h
@@ -1,8 +1,30 @@
 #ifndef __MACDEF_H__
 #define __MACDEF_H__ 
 
+/* Contain variety of Define for cPlusPlus
+*/
+
 namespace plusFCL_BTL
 {
+	/*Export class Instance for static or Dynamic
+	*/
+#define __DECLAREEXPORTCLASS(className)\
+className* get##className##Instance();
+
+#define __IMPLEMENTEXPORTCLASS(className)\
+className* get##className##Instance() {\
+return new className; }
+
+#define __DECLAREEXPORTCLASSMUTIL(className,Instance)\
+className* get##className##Instance(Instance);
+
+#define __IMPLEMENTEXPORTCLASSMUTIL(className,Instance)\
+className* get##className##Instance(Instance) {\
+return new className(Instance); }
+
+#define MAXPATHLEN 128
+
+
   /* @Detail: InI File
   */
 	
@@ -14,8 +36,6 @@ namespace plusFCL_BTL
   
   /* @Block Mgr
   */
-
-  
 }
 
 #endif
